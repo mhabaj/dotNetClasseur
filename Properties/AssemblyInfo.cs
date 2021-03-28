f10990e3820f5aaecf805cbd0463bfb5d9635030
ALHABAJ Mahmod <mhabaj99@hotmail.com>
2021-03-28 20:53:26 +0200

    merge avec sean
    e

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("0b5b50b9-41c8-42aa-85da-8d6197345369")]
+[assembly: Guid("794ea797-c71b-4e1f-8b34-340e357c4b21")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
